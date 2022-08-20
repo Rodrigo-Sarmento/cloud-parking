7f539ef9d0ee1bb6febb9365ef057f6470cdeda3
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-08-19 23:32:25 -0300

    initial

---
+++ src/main/java/digo/me/parking/controller/HelloController.java
@@ -0,0 +1,15 @@
+package digo.me.parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String Hello(){
+        return "Hello java spring";
+    }
+}
---
+++ src/test/java/digo/me/parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package digo.me.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
